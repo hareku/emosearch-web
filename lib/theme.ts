0489468b2354f1989551ceda9664b3d85c689fce
hareku <hareku908@gmail.com>
2020-11-09 10:57:27 +0900

    Update theme

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,16 +1,12 @@
 import { createMuiTheme } from "@material-ui/core/styles"
-import red from "@material-ui/core/colors/red"
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#556cd6",
+      main: "#2196f3",
     },
     secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
+      main: "#f50057",
     },
     background: {
       default: "#fff",
